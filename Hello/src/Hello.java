136504ee28cc6223d0f55ef4c7a31ca8510315d6
George Radcliff <george.radcliff@pcc.edu>
2017-05-02 18:15:32 -0700

    added print date

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,9 +1,11 @@
 /**
  * Created by George Radcliff on 5/2/2017.
  */
+import java.util.Date;
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Goodnight World");
+        System.out.println(new Date());
     }
 }
